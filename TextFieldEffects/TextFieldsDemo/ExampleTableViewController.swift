4bd312aa7213690825e2b3a955b74cd5c317596e
Raul Riera <rieraraul@gmail.com>
2015-08-28 15:48:10 +0100

    Added a way to see all the textfields getting pre populated with data

---
+++ TextFieldEffects/TextFieldsDemo/ExampleTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExampleTableViewController.swift
+//  TextFieldEffects
+//
+//  Created by Raúl Riera on 28/08/2015.
+//  Copyright © 2015 Raul Riera. All rights reserved.
+//
+
+import UIKit
+import TextFieldEffects
+
+class ExampleTableViewController : UITableViewController {
+    
+    @IBOutlet private var textFields: [TextFieldEffects]!
+    
+    /**
+    Set this value to true if you want to see all the "firstName"
+    textFields prepopulated with the name "Raul" (for testing purposes)
+    */
+    let prefillTextField = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard prefillTextField == true else { return }
+        
+        _ = textFields.map { $0.text = "Raul" }
+    }
+}
