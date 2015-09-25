84a0f350050ebb427018b222f7c049002a9dc50d
Raul Riera <rieraraul@gmail.com>
2015-09-25 22:59:28 +0100

    Property observing `text` so animations can occur when this is updated. Also intense documentation

--- TextFieldEffects/TextFieldEffects/TextFieldsEffects.swift
+++ TextFieldEffects/TextFieldEffects/TextFieldsEffects.swift
@@ -10,33 +10,49 @@ import Foundation
 import UIKit
 
 extension String {
+    /**
+    true iff self contains characters.
+    */
     public var isNotEmpty: Bool {
         return !isEmpty
     }
 }
 
+/**
+A TextFieldEffects object is a control that displays editable text and contains the boilerplates to setup unique animations for text entrey and display. You typically use this class the same way you use UITextField.
+*/
 public class TextFieldEffects : UITextField {
     
+    /**
+    UILabel that holds all the placeholder information
+    */
     public let placeholderLabel = UILabel()
     
+    /**
+    Creates all the animations that are used to leave the textfield in the "entering text" state.
+    */
     public func animateViewsForTextEntry() {
         fatalError("\(__FUNCTION__) must be overridden")
     }
     
+    /**
+    Creates all the animations that are used to leave the textfield in the "display input text" state.
+    */
     public func animateViewsForTextDisplay() {
         fatalError("\(__FUNCTION__) must be overridden")
     }
     
+    /**
+    Draws the receiver’s image within the passed-in rectangle.
+    
+    - parameter rect:	The portion of the view’s bounds that needs to be updated.
+    */
     public func drawViewsForRect(rect: CGRect) {
         fatalError("\(__FUNCTION__) must be overridden")
     }
     
     public func updateViewsForBoundsChange(bounds: CGRect) {
         fatalError("\(__FUNCTION__) must be overridden")
-    }
-    
-    override public func prepareForInterfaceBuilder() {
-        drawViewsForRect(frame)
     }
     
     // MARK: - Overrides
@@ -47,6 +63,16 @@ public class TextFieldEffects : UITextField {
     
     override public func drawPlaceholderInRect(rect: CGRect) {
         // Don't draw any placeholders
+    }
+    
+    override public var text: String? {
+        didSet {
+            if let text = text where text.isNotEmpty {
+                animateViewsForTextEntry()
+            } else {
+                animateViewsForTextDisplay()
+            }
+        }
     }
     
     // MARK: - UITextField Observing
@@ -61,12 +87,23 @@ public class TextFieldEffects : UITextField {
         }
     }
     
+    /**
+    The textfield has started an editing session.
+    */
     public func textFieldDidBeginEditing() {
         animateViewsForTextEntry()
     }
     
+    /**
+    The textfield has ended an editing session.
+    */
     public func textFieldDidEndEditing() {
         animateViewsForTextDisplay()
     }
     
+    // MARK: - Interface Builder
+    
+    override public func prepareForInterfaceBuilder() {
+        drawViewsForRect(frame)
+    }
 }
