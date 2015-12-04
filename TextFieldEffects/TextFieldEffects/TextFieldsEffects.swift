2607eb666c991bdc3f878c0588de06150e61b8ca
Patrick Reynolds <patrickscottreynolds@gmail.com>
2015-12-03 16:03:07 -0800

    Fix minor typo with more explicit reference

--- TextFieldEffects/TextFieldEffects/TextFieldsEffects.swift
+++ TextFieldEffects/TextFieldEffects/TextFieldsEffects.swift
@@ -10,7 +10,7 @@ import UIKit
 
 extension String {
     /**
-    true if self contains characters.
+    true Iff self contains characters.
     */
     public var isNotEmpty: Bool {
         return !isEmpty
