ce4a8e61965f9dddc755818bf6dcd1798426f949
Sander Theeuwes <info@theeuwes-it.nl>
2020-08-22 09:50:02 +0200

    [FIX] Fix PR comments

--- TextFieldEffects/TextFieldsDemo/ExampleViewController.swift
+++ TextFieldEffects/TextFieldsDemo/ExampleViewController.swift
@@ -10,25 +10,7 @@ import UIKit
 
 class ExampleViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    
     @IBAction func endEditing() {
         view.endEditing(false)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
